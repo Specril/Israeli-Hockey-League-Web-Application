40d6db6d61a092e9a392b71fd4ea412aa26b3e2a
Specril <81250648+Specril@users.noreply.github.com>
2024-06-16 20:36:17 +0300

    Create SQLQuery_goals_count.sql

---
+++ SQL_queries/SQLQuery_goals_count.sql
@@ -0,0 +1,9 @@
+-- Displaying how many goals each player scored
+
+
+SELECT Full_Name AS שם_השחקן, כמות_גולים from
+Users INNER JOIN
+(SELECT Goals.User_ID, count(*) as כמות_גולים
+from Goals
+group by User_ID) AS T1 on Users.User_ID=T1.User_ID
+order by כמות_גולים DESC;
