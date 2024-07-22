1d85d79bb9080ea65fa2abc883427399a3f5b38d
Naama <naama.gerner@campus.technion.ac.il>
2024-07-22 23:09:35 +0300

    feature: update APIs

--- israeli-hockey-website/src/app/api/form_manage_photo/route.js
+++ israeli-hockey-website/src/app/api/form_manage_photo/route.js
@@ -5,10 +5,9 @@ import sql from 'mssql';
 export async function POST(req, res) {
     const {Photo} = await req.json();
     try {
-        console.log(Photo)
         const pool = await getConnection();
         let getMaxPhotoID = await pool.request()
-            .query('SELECT MAX(Photo_ID) AS max_Photo_ID FROM photos');
+            .query('SELECT MAX(Photo_ID) AS max_Photo_ID FROM Photos');
         let maxPhotoID = getMaxPhotoID.recordset[0].max_Photo_ID;
         let Photo_ID = maxPhotoID + 1;
         console.log(maxPhotoID)
@@ -19,11 +18,26 @@ export async function POST(req, res) {
         await pool.request()
             .input('Photo_ID',sql.Int, Photo_ID)
             .input('Photo', sql.Text, Photo)
-            .query('INSERT INTO photos (Photo_ID, Photo) VALUES (@Photo_ID, @Photo)')
+            .query('INSERT INTO Photos (Photo_ID, Photo) VALUES (@Photo_ID, @Photo)')
         return NextResponse.json({ success: true});
         
     } catch (error) {
       console.error('Database operation failed: ', error);
       return NextResponse.json({ success: false});
+    }
+}
+
+export async function DELETE(req, res) {
+    const { Photo_ID } = await req.json();
+    try {
+        const pool = await getConnection();
+        await pool.request()
+            .input('Photo_ID', sql.Int, Photo_ID)
+            .query('DELETE FROM Photos WHERE Photo_ID = @Photo_ID');
+        return NextResponse.json({ success: true });
+
+    } catch (error) {
+        console.error('Database operation failed: ', error);
+        return NextResponse.json({ success: false });
     }
 }
--- israeli-hockey-website/src/app/api/form_results_updating/route.js
+++ israeli-hockey-website/src/app/api/form_results_updating/route.js
@@ -3,7 +3,7 @@ import { getConnection } from "@/app/lib/db";
 import sql from 'mssql';
 
 export async function POST(req, res) {
-    const {Game_ID, Goals, Penalties} = await req.json();
+    const {Game_ID, Goals, Penalties, Cards} = await req.json();
     try {
       const pool = await getConnection();
       for (const goal of Goals){
@@ -34,6 +34,21 @@ export async function POST(req, res) {
             .input('Team_ID', sql.Int, penalty['Team_ID'])
             .query('INSERT INTO Penalties (Penalty_ID, Game_ID, User_ID, Time_Stamp, Team_ID) VALUES (@Penalty_ID, @Game_ID, @User_ID, @Time_Stamp, @Team_ID)')
       }
+      for (const card of Cards){
+        // insert each Card to Cards table
+        let getMaxCardID = await pool.request()
+        .query('SELECT MAX(Card_ID) AS max_Card_ID FROM Cards');
+        let maxCardID = getMaxCardID.recordset[0].max_Card_ID;
+        let newCardId = maxCardID + 1;
+          await pool.request()
+            .input('Card_ID',sql.Int, newCardId)
+            .input('Game_ID',sql.Int, Game_ID)
+            .input('User_ID',sql.Int, card['User_ID'])
+            .input('Time_Stamp', sql.VarChar, card['Time_Stamp'])
+            .input('Team_ID', sql.Int, card['Team_ID'])
+            .input('Card_Type', sql.VarChar, card['Card_Type'])
+            .query('INSERT INTO Cards (Card_ID, Game_ID, User_ID, Time_Stamp, Team_ID, Card_Type) VALUES (@Card_ID, @Game_ID, @User_ID, @Time_Stamp, @Team_ID, @Card_Type)')
+    }
       return NextResponse.json({ success: true});
       
 
