b988ebe08341e812576f126116aa5dd9014bc376
Naama <naama.gerner@campus.technion.ac.il>
2024-07-24 12:16:46 +0300

    feature: update APIs and add fetch API

---
+++ israeli-hockey-website/src/app/api/fetch/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { getConnection } from "@/app/lib/db";
+
+export async function POST(req, res) {
+  try {
+    const { query } = await req.json();
+    if (!query) {
+      return NextResponse.json({ success: false, message: 'Query is missing' });
+    }
+    const pool = await getConnection();
+    const result = await pool.request()
+        .query(query);
+
+    console.log(result.recordset)
+    return NextResponse.json(result.recordset);
+
+  } catch (error) {
+    console.error('Database operation failed: ', error);
+    return NextResponse.json({ success: false, message: 'Failed to fetch the data' });
+  }
+}
--- israeli-hockey-website/src/app/api/form_add_team/route.js
+++ israeli-hockey-website/src/app/api/form_add_team/route.js
@@ -3,7 +3,7 @@ import { getConnection } from "@/app/lib/db";
 import sql from 'mssql';
 
 export async function POST(req, res) {
-    let {Team_Name, Rank, Location_ID, League_ID, Location_Name} = await req.json();
+    let {Team_Name, Rank, Location_ID, Age, Location_Name} = await req.json();
     try {
         const pool = await getConnection();
         if (!Location_ID && Location_Name){
@@ -28,8 +28,8 @@ export async function POST(req, res) {
             .input('Team_Name', sql.NVarChar, Team_Name)
             .input('Rank', sql.NVarChar, Rank)
             .input('Location_ID', sql.Int, Location_ID)
-            .input('League_ID', sql.Int, League_ID)
-            .query('INSERT INTO Teams (Team_ID, Team_Name, Rank, Location_ID, League_ID) VALUES (@Team_ID, @Team_Name, @Rank, @Location_ID, @League_ID)')
+            .input('Age', sql.NVarChar, Age)
+            .query('INSERT INTO Teams (Team_ID, Team_Name, Rank, Location_ID, Age) VALUES (@Team_ID, @Team_Name, @Rank, @Location_ID, @Age)')
         return NextResponse.json({ success: true});
 
     } catch (error) {
