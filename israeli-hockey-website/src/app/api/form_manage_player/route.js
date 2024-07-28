e4857eb1810b626efd7169f1845c574d975cb011
Naama <naama.gerner@campus.technion.ac.il>
2024-07-28 23:36:04 +0300

    update api for form add player

--- israeli-hockey-website/src/app/api/form_manage_player/route.js
+++ israeli-hockey-website/src/app/api/form_manage_player/route.js
@@ -3,23 +3,9 @@ import { getConnection } from "@/app/lib/db";
 import sql from 'mssql';
 
 export async function POST(req, res) {
-    const {User_ID, Team_ID, Position, Shirt_Number, Experience, Residence} = await req.json();
+    const {User_ID, Team_ID, Position, Shirt_Number} = await req.json();
     try {
         const pool = await getConnection();
-        let result = await pool.request()
-            .input('User_ID', sql.Int, User_ID)
-            .query('SELECT COUNT(*) as count FROM UsersPlayers WHERE User_ID = @User_ID');
-
-        if (result.recordset[0].count === 0){
-            // insert the user to UsersPlayer table
-            const Date_Created = new Date();
-            await pool.request()
-            .input('User_ID',sql.Int, User_ID)
-            .input('Date_Created', sql.Date, Date_Created)
-            .input('Experience', sql.NVarChar, Experience)
-            .input('Residence', sql.NVarChar, Residence)
-            .query('INSERT INTO UsersPlayers (User_ID, Date_Created, Experience, Residence) VALUES (@User_ID, @Date_Created, @Experience, @Residence)')
-        }
         // insert the player to PlayersInTeams table
         await pool.request()
             .input('User_ID',sql.Int, User_ID)
