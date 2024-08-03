87cf4d2050911a7ea537bc3cbdb833d081ed94f7
zziivv1 <zziivv1@gmail.com>
2024-08-03 12:14:43 +0300

    .

--- israeli-hockey-website/src/app/NavBar.js
+++ israeli-hockey-website/src/app/NavBar.js
@@ -1,57 +1,54 @@
 "use client";
 
-import React from "react";
 import "./style.css";
-import { Menu, Dropdown, Button } from "antd";
-import { DownOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { toggleLoginForm } from "./Login";
+import { Menu, Dropdown, Button } from 'antd';
 
 export default function NavBar() {
-  const fanZoneMenu = (
-    <Menu>
-      <Menu.Item key="1">
-        <a href="#fan-zone">אזור האוהדים</a>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <a href="#educational-resources">משאבים הדרכתיים</a>
-      </Menu.Item>
-      <Menu.Item key="3">
-        <a href="#interactive-challenges">אתגרים אינטראקטיביים</a>
-      </Menu.Item>
-      <Menu.Item key="4">
-        <a href="#contact-us">צור קשר</a>
-      </Menu.Item>
-    </Menu>
-  );
-
-  const managementMenu = (
+  const gamesMenu = (
     <Menu>
       <Menu.Item key="1">
         <a href="form_add_game">הוספת משחק</a>
       </Menu.Item>
       <Menu.Item key="2">
-        <a href="form_personal_details">עדכון פרטים אישיים</a>
+        <a href="form_delete_game">מחיקת משחקים</a>
       </Menu.Item>
       <Menu.Item key="3">
-        <a href="form_add_player">הוספת שחקן</a>
-      </Menu.Item>
-      <Menu.Item key="4">
-        <a href="form_add_team">הוספת קבוצה</a>
-      </Menu.Item>
-      <Menu.Item key="5">
         <a href="form_results_updating">העלאת תוצאות משחק</a>
       </Menu.Item>
-      <Menu.Item key="6">
-        <a href="form_delete_player">מחיקת שחקן</a>
+      <Menu.Item key="4">
+        <a href="form_edit_game_results">עריכת תוצאות משחק</a>
       </Menu.Item>
-      <Menu.Item key="7">
+    </Menu>
+  );
+
+  const teamsMenu = (
+    <Menu>
+      <Menu.Item key="1">
+        <a href="form_add_player">הוספת שחקן לקבוצה</a>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <a href="form_delete_player">מחיקת שחקן מקבוצה</a>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <a href="form_add_team">הוספת קבוצה חדשה</a>
+      </Menu.Item>
+      <Menu.Item key="4">
         <a href="form_delete_team">מחיקת קבוצה</a>
+      </Menu.Item>
+      <Menu.Item key="5">
+        <a href="form_add_team_to_league">הוספת קבוצה לליגה</a>
+      </Menu.Item>
+      <Menu.Item key="6">
+        <a href="form_delete_team_from_league">מחיקת קבוצה מליגה</a>
       </Menu.Item>
     </Menu>
   );
 
   return (
     <nav>
-      <ul className="nav-menu">
+      <ul>
         <li>
           <a href="/">ראשי</a>
         </li>
@@ -68,71 +65,23 @@ export default function NavBar() {
           <a href="browse_teams">סקירת קבוצות</a>
         </li>
         <li>
-          <Dropdown overlay={fanZoneMenu} trigger={['click']}>
-            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-              אזור האוהדים <DownOutlined />
-            </a>
+          <Dropdown overlay={gamesMenu}>
+            <a href="#">ניהול משחקים</a>
           </Dropdown>
         </li>
         <li>
-          <Dropdown overlay={managementMenu} trigger={['click']}>
-            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-              ניהול <DownOutlined />
-            </a>
+          <Dropdown overlay={teamsMenu}>
+            <a href="#">ניהול קבוצות</a>
           </Dropdown>
         </li>
-        <li>
-          <a href="#interactive-challenges">אתגרים אינטראקטיביים</a>
-        </li>
-        <li>
-          <a href="#contact-us">צור קשר</a>
-        </li>
-        <li>
-          <a href="form_add_game">הוספת משחק</a>
-        </li>
-        <li>
-          <a href="form_delete_game">מחיקת משחקים</a>
-        </li>
-        <li>
-          <a href="form_results_updating">העלאת תוצאות משחק</a>
-        </li>
-        <li>
-          <a href="form_edit_game_results">עריכת תוצאות משחק</a>
-        </li>
-        <li>
-          <a href="form_personal_details">עדכון פרטים אישיים</a>
-        </li>
-
-        <li>
-          <a href="form_add_player">הוספת שחקן לקבוצה</a>
-        </li>
-        <li>
-          <a href="form_delete_player">מחיקת שחקן מקבוצה</a>
-        </li>
-        <li>
-          <a href="form_add_team">הוספת קבוצה חדשה</a>
-        </li>
-
-        <li>
-          <a href="form_delete_team">מחיקת קבוצה</a>
-        </li>
-
-        <li>
-          <a href="form_add_team_to_league">הוספת קבוצה לליגה</a>
-        </li>
-
-        <li>
-          <a href="form_delete_team_from_league"> מחיקת קבוצה מליגה</a>
-        </li>
-
-        
         <li>
           <a href="form_user_clasification">סיווג יוזר</a>
         </li>
         <li>
-          <a href="#login" className="login-button" onClick={toggleLoginForm}>
-            התחברות
-          </a>
+          <a href="form_personal_details">עדכון פרטים אישיים</a>
+        </li>
+        <li>
+          <a href="authentication">התחברות</a>
         </li>
       </ul>
     </nav>
