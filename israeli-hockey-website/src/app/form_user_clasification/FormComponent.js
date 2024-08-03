d711fd5c784b8e2f2fcee2f0524603b59ceab46a
Shahar Blas <126664286+ShaharBlas@users.noreply.github.com>
2024-08-03 17:53:16 +0300

    udating clasification users

--- israeli-hockey-website/src/app/form_user_clasification/FormComponent.js
+++ israeli-hockey-website/src/app/form_user_clasification/FormComponent.js
@@ -1,6 +1,5 @@
-"use client";
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Typography, Row, Col, Select, Checkbox, Descriptions, Input, message } from 'antd';
+import { Form, Button, Typography, Row, Col, Select, Checkbox, Input, message } from 'antd';
 import 'antd/dist/reset.css';
 import "../style.css";
 
@@ -11,14 +10,15 @@ const { TextArea } = Input;
 export default function FormComponent({ data }) {
   const initialFormState = {
     User_ID: '',
-    Clasification: [], // Ensure Clasification is always an array
-    CustomRole: '' // Add custom role to form state
+    Clasification: [],
+    CustomRole: ''
   };
 
   const fieldLabels = {
     User_ID: 'שם מלא',
-    Clasification: 'סיווג המשתמש',
-    CustomRole: 'מאמן- תפקיד' // Label for custom role
+    Clasification: 'בחר סיווגים שתרצה להוסיף למשתמש',
+    CustomRole: 'מאמן- תפקיד',
+    ClasificationsToDelete: 'בחר סיווגים שתרצה להוריד למשתמש'
   };
 
   const usersOptions = data[0]; // User options
@@ -28,7 +28,7 @@ export default function FormComponent({ data }) {
     { key: 'Users-Players', value: 'שחקן' },
     { key: 'Users-Fans', value: 'אוהד' },
     { key: 'Users-Coaches', value: 'מאמן' }
-  ]; // Predefined roles
+  ];
 
   const roleKeys = {
     'Users_Admin': 'admin',
@@ -38,13 +38,16 @@ export default function FormComponent({ data }) {
     'Users-Coaches': 'coach'
   };
 
-  // Additional state to hold selected user details and custom role
   const [selectedUser, setSelectedUser] = useState(null);
   const [customRole, setCustomRole] = useState('');
-
+  const [userTables, setUserTables] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
   const [isClient, setIsClient] = useState(false);
   const [form] = Form.useForm();
+  const [availableClasifications, setAvailableClasifications] = useState([]);
+  const [selectedClasificationsToAdd, setSelectedClasificationsToAdd] = useState([]);
+  const [selectedClasificationsToDelete, setSelectedClasificationsToDelete] = useState([]);
+  const [userClasifications, setUserClasifications] = useState([]);
 
   useEffect(() => {
     setIsClient(true);
@@ -60,6 +63,64 @@ export default function FormComponent({ data }) {
     }
   }, [formData, isClient]);
 
+  const fetchUserTables = async (userId) => {
+    let tables = [];
+    const queries = [
+      `SELECT N'אדמין' as Table_Name FROM UsersAdmin WHERE User_ID = '${userId}';`,
+      `SELECT N'שופט' as Table_Name FROM UsersReferees WHERE User_ID = '${userId}';`,
+      `SELECT N'שחקן' as Table_Name FROM UsersPlayers WHERE User_ID = '${userId}';`,
+      `SELECT N'אוהד' as Table_Name FROM UsersFans WHERE User_ID = '${userId}';`,
+      `SELECT N'מאמן' as Table_Name FROM UsersCoaches WHERE User_ID = '${userId}';`
+    ];
+
+    try {
+      for (const query of queries) {
+        const response = await fetch('/api/fetch', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ query }),
+        });
+
+        if (!response.ok) throw new Error('Network response was not ok');
+        const result = await response.json();
+        if (result.length > 0) tables.push(result[0].Table_Name);
+      }
+    } catch (error) {
+      console.error('Error fetching user tables:', error);
+    }
+
+    return tables;
+  };
+
+  const fetchUserClasifications = async (userId) => {
+    let clasifications = [];
+    const queries = [
+      `SELECT 'Users_Admin' as Clasification FROM UsersAdmin WHERE User_ID = '${userId}';`,
+      `SELECT 'Users-Referees' as Clasification FROM UsersReferees WHERE User_ID = '${userId}';`,
+      `SELECT 'Users-Players' as Clasification FROM UsersPlayers WHERE User_ID = '${userId}';`,
+      `SELECT 'Users-Fans' as Clasification FROM UsersFans WHERE User_ID = '${userId}';`,
+      `SELECT 'Users-Coaches' as Clasification FROM UsersCoaches WHERE User_ID = '${userId}';`
+    ];
+
+    try {
+      for (const query of queries) {
+        const response = await fetch('/api/fetch', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ query }),
+        });
+
+        if (!response.ok) throw new Error('Network response was not ok');
+        const result = await response.json();
+        if (result.length > 0) clasifications.push(result[0].Clasification);
+      }
+    } catch (error) {
+      console.error('Error fetching user classifications:', error);
+    }
+
+    return clasifications;
+  };
+
   const handleChange = (changedValues) => {
     setFormData((prevData) => ({
       ...prevData,
@@ -67,29 +128,46 @@ export default function FormComponent({ data }) {
     }));
   };
 
-  const handleSelectChange = (value, field) => {
+  const handleSelectChange = async (value, field) => {
     setFormData((prevData) => ({
       ...prevData,
       [field]: value,
     }));
 
-    // Find and set the selected user details
     const user = usersOptions.find(user => user.key === value);
     setSelectedUser(user || null);
+
+    if (user) {
+      const tables = await fetchUserTables(user.key);
+      setUserTables(tables);
+
+      const availableClasifications = clasificationOptions
+        .filter(option => !tables.includes(option.value)) // Filter based on tables user is not in
+        .map(option => ({ label: option.value, value: option.key }));
+
+      setAvailableClasifications(availableClasifications);
+
+      // Fetch user classifications
+      const clasifications = await fetchUserClasifications(user.key);
+      const userClasificationsWithLabels = clasifications
+        .map(clas => clasificationOptions.find(option => option.key === clas))
+        .filter(option => option)
+        .map(option => ({ label: option.value, value: option.key }));
+      
+      setUserClasifications(userClasificationsWithLabels);
+    } else {
+      setUserTables([]);
+      setAvailableClasifications([]);
+      setUserClasifications([]);
+    }
   };
 
-  const handleCheckboxChange = (checkedValues) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      Clasification: checkedValues,
-    }));
-    
-    // Check if 'Users-Coaches' is selected and manage custom role input visibility
-    if (checkedValues.includes('Users-Coaches')) {
-      setCustomRole(formData.CustomRole); // Preserve custom role
-    } else {
-      setCustomRole(''); // Clear custom role if not selected
-    }
+  const handleCheckboxChangeToAdd = (checkedValues) => {
+    setSelectedClasificationsToAdd(checkedValues);
+  };
+
+  const handleCheckboxChangeToDelete = (checkedValues) => {
+    setSelectedClasificationsToDelete(checkedValues);
   };
 
   const handleCustomRoleChange = (e) => {
@@ -100,178 +178,185 @@ export default function FormComponent({ data }) {
     }));
   };
 
-  const handleSubmit = async () => {
-    if (formData.Clasification.length === 0) {
-      message.error('Please pick at least one classification');
+  const handleAdd = async () => {
+    if (selectedClasificationsToAdd.length === 0) {
+      message.error('בחר לפחות סיווג אחד');
       return;
     }
 
-    // Prepare role data
     const roleData = clasificationOptions.reduce((acc, role) => {
-      acc[roleKeys[role.key]] = formData.Clasification.includes(role.key) ? 1 : 0;
+      acc[roleKeys[role.key]] = selectedClasificationsToAdd.includes(role.key) ? 1 : 0;
       return acc;
     }, {});
-  
-    // Prepare final data to be sent
+
     const finalData = {
       User_ID: formData.User_ID,
       ...roleData,
       CustomRole: formData.Clasification.includes('Users-Coaches') ? formData.CustomRole : ''
     };
-  
+
     try {
       const response = await fetch('/api/manage_users_types', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(finalData), // Ensure CustomRole is sent as a string
+        body: JSON.stringify(finalData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      message.success('Classifications added successfully');
+      handleClearAll(); // Clear form after successful addition
+    } catch (error) {
+      console.error('Error adding classifications:', error);
+      message.error('Error adding classifications');
+    }
+  };
+
+  const handleDelete = async () => {
+    if (!formData.User_ID) {
+      message.error('בחר משתמש');
+      return;
+    }
+  
+    if (selectedClasificationsToDelete.length === 0) {
+      message.error('בחר סיווג אחד לפחות');
+      return;
+    }
+  
+    // Construct the role data based on selected classifications to delete
+    const roleData = clasificationOptions.reduce((acc, role) => {
+      acc[roleKeys[role.key]] = selectedClasificationsToDelete.includes(role.key) ? 1 : 0;
+      return acc;
+    }, {});
+  
+    const finalData = {
+      User_ID: formData.User_ID,
+      ...roleData
+    };
+  
+    try {
+      const response = await fetch('/api/manage_users_types', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(finalData),
       });
   
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      
-      message.success('Form submitted successfully');
-    } catch (error) {
-      console.error('Error submitting form:', error);
-      message.error('Error updating data');
-    }
-  };
   
-  const handleClearAll = () => {
-    form.resetFields();
-    setFormData(initialFormState);
-    setSelectedUser(null); // Clear selected user details
-    setCustomRole(''); // Clear custom role
-    if (isClient) {
-      localStorage.removeItem('formData');
+      message.success('Classifications deleted successfully');
+      handleClearAll(); // Clear form after successful deletion
+    } catch (error) {
+      console.error('Error deleting classifications:', error);
+      message.error('Error deleting classifications');
     }
   };
 
-  useEffect(() => {
-    if (isClient) {
-      form.setFieldsValue({
-        ...formData,
-        CustomRole: customRole // Sync custom role with form
-      });
-    }
-  }, [formData, form, customRole, isClient]);
+  const handleClearAll = () => {
+    setFormData(initialFormState);
+    setSelectedClasificationsToAdd([]);
+    setSelectedClasificationsToDelete([]);
+    setCustomRole('');
+  };
 
   return (
-    <div style={{ maxWidth: '800px', margin: '0 auto', padding: '20px' }}>
-      <Title level={3}> סיווג המשתמשים ונתינת הרשאות</Title>
+    <div className='form-container'>
       <Form
         form={form}
         layout="vertical"
         initialValues={formData}
         onValuesChange={handleChange}
-        onFinish={handleSubmit}
+        style={{ maxWidth: 600, margin: '0 auto' }}
       >
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item
-              label={fieldLabels['User_ID']}
-              name="User_ID"
-              rules={[
-                {
-                  required: true,
-                  message: `${fieldLabels['User_ID']} is required`,
-                },
-              ]}
-            >
-              <Select
-                showSearch
-                value={formData['User_ID']}
-                onChange={(value) => handleSelectChange(value, 'User_ID')}
-                filterOption={(input, option) =>
-                  option.props.children
-                    ? option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                    : false
-                }
-              >
-                {usersOptions.map((option) => (
-                  <Option key={option.key} value={option.key}>
-                    {option.value}
-                  </Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-
-          <Col span={12}>
-            <Form.Item
-              label={fieldLabels['Clasification']}
-              name="Clasification"
-            >
-              <Checkbox.Group
-                value={formData['Clasification']}
-                onChange={handleCheckboxChange}
-              >
-                <Row>
-                  {clasificationOptions.map(role => (
-                    <Col span={8} key={role.key}>
-                      <Checkbox value={role.key}>{role.value}</Checkbox>
-                    </Col>
-                  ))}
-                </Row>
-              </Checkbox.Group>
-            </Form.Item>
-          </Col>
-        </Row>
-
-        {/* Display custom role input if 'Users-Coaches' is selected */}
-        {(formData.Clasification || []).includes('Users-Coaches') && (
-          <Form.Item
-            label={fieldLabels['CustomRole']}
-            name="CustomRole"
+        <Form.Item label={fieldLabels.User_ID}>
+          <Select
+            placeholder="בחר משתמש"
+            value={formData.User_ID}
+            onChange={(value) => handleSelectChange(value, 'User_ID')}
           >
-            <TextArea
-              value={customRole}
-              onChange={handleCustomRoleChange}
-              placeholder="הכנס את תפקיד המאמן"
-              rows={4}
-            />
-          </Form.Item>
-        )}
+            {usersOptions.map((user) => (
+              <Option key={user.key} value={user.key}>
+                {user.value}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
 
-        {/* Display selected user details */}
         {selectedUser && (
-          <Row gutter={16}>
-            <Col span={24}>
-              <Title level={4}>פרטי המשתמש הנבחר</Title>
-              <Descriptions bordered>
-                <Descriptions.Item label="שם מלא" span={3}>
-                  {selectedUser.value}
-                </Descriptions.Item>
-                <Descriptions.Item label="סיווג המשתמש (ניתן לבחור כמה אפשרויות)" span={3}>
-                  {formData.Clasification.map(roleKey => {
-                    const role = clasificationOptions.find(r => r.key === roleKey);
-                    return role ? role.value : null;
-                  }).join(', ')}
-                </Descriptions.Item>
-                {(formData.Clasification || []).includes('Users-Coaches') && (
-                  <Descriptions.Item label="מאמן- תפקיד" span={3}>
-                    {formData.CustomRole}
-                  </Descriptions.Item>
-                )}
-              </Descriptions>
-            </Col>
-          </Row>
-        )}
+          <>
+            <Form.Item label={fieldLabels.Clasification}>
+              <Checkbox.Group
+                options={availableClasifications}
+                value={selectedClasificationsToAdd}
+                onChange={handleCheckboxChangeToAdd}
+              />
+            </Form.Item>
 
-        <Row gutter={16}>
-          <Col span={12}>
-            <Button type="primary" htmlType="submit" block>
-              Send
-            </Button>
-          </Col>
-          <Col span={12}>
-            <Button type="default" onClick={handleClearAll} block>
-              Clear All
-            </Button>
-          </Col>
-        </Row>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Button
+                  type="primary"
+                  onClick={handleAdd}
+                >
+                  הוסף סיווגים
+                </Button>
+              </Col>
+              <Col span={12}>
+                <Button
+                  type="default"
+                  onClick={handleClearAll}
+                >
+                  ניקוי
+                </Button>
+              </Col>
+            </Row>
+
+            <Form.Item label={fieldLabels.ClasificationsToDelete}>
+              <Checkbox.Group
+                options={userClasifications}
+                value={selectedClasificationsToDelete}
+                onChange={handleCheckboxChangeToDelete}
+              />
+            </Form.Item>
+
+            <Row gutter={16}>
+              <Col span={12}>
+                <Button
+                  type="danger"
+                  onClick={handleDelete}
+                >
+                  מחק סיווגים
+                </Button>
+              </Col>
+              <Col span={12}>
+                <Button
+                  type="default"
+                  onClick={handleClearAll}
+                >
+                  ניקוי
+                </Button>
+              </Col>
+            </Row>
+
+            {formData.Clasification.includes('Users-Coaches') && (
+              <Form.Item label={fieldLabels.CustomRole}>
+                <TextArea
+                  value={customRole}
+                  onChange={handleCustomRoleChange}
+                  placeholder="הזן תפקיד מותאם אישית"
+                  rows={4}
+                />
+              </Form.Item>
+            )}
+          </>
+        )}
       </Form>
     </div>
   );
